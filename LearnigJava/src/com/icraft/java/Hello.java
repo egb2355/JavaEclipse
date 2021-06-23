0567dd6d27265bec5b3d8c2d0431382b32d3c193
egb2355 <ermiasgetuo@gmail.com>
2021-06-23 00:00:53 -0400

    add some program!

---
+++ LearnigJava/src/com/icraft/java/Hello.java
@@ -0,0 +1,11 @@
+package com.icraft.java;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Welcome to java");
+
+	}
+
+}
